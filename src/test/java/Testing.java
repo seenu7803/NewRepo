60f67fbd99714a50da2d37402861e28d1cd33db6
itvaliant <itvaliant@outlook.com>
2016-08-28 18:08:06 +0530

    Update Testing.java

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -1,14 +1,37 @@
 package com.cloudbees.aws.demo;
+import junit.framework.Test;
 import junit.framework.TestCase;
-import org.junit.Assert;
-import org.junit.Test;
-public class Testing extends TestCase {
+import junit.framework.TestSuite;
 
-@Test 
-public void Test()
-  {
-    System.out.println("testing done");
-     Assert.assertEquals(5, 5);
-  }
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
 
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
 }
