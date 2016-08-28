eafb64a92b1a673c4fafd9506556ecb3199efbab
itvaliant <itvaliant@outlook.com>
2016-08-28 18:54:01 +0530

    Create TestSelenium.java

---
+++ src/test/java/TestSelenium.java
@@ -0,0 +1,27 @@
+package JavaSelenium;		
+
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;	
+
+public class TestSelenium {		
+	    private WebDriver driver;		
+		@Test				
+		public void testEasy() {	
+			driver.get("http://54.197.163.83/demo/");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Hello AWS")); 		
+		}	
+		@BeforeTest
+		public void beforeTest() {	
+		    driver = new FirefoxDriver();  
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+}	
