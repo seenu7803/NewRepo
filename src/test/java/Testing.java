824df09a80b28500d25b6a9fe5326de93ac8a610
itvaliant <itvaliant@outlook.com>
2016-08-28 17:42:47 +0530

    Update Testing.java

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -1,6 +1,6 @@
 package com.cloudbees.aws.demo;
 import junit.framework.TestCase;
-
+import static org.junit.Assert.assertEquals;
 public class Testing extends TestCase {
 
 @Test public void Test()
